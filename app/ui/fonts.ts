1cf72df12ea7f644f9ada8900eb36aaad0987e40
Pedro Steven Castiblanco <pcastiblanco@megasoft.com.co>
2024-06-26 10:20:55 -0500

    TASK(DashBoard): Create pages, layout and navigating

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Lusitana, Inter, Montserrat } from "next/font/google";
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const montserrat = Montserrat({ subsets: ['latin'] });
